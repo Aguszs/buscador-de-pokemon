1cf5124306b3167a3d2656d2e05228198e39da44
aguszs <tucorreodegithub>
2022-12-13 07:42:59 -0300

    first commit

---
+++ js/request.js
@@ -0,0 +1,13 @@
+
+const requestCity = async (city) => {
+    const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+    const queryParams = `${city}`;
+
+    try {
+        const response = await fetch(baseURL + queryParams);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        showError();
+    }
+};
